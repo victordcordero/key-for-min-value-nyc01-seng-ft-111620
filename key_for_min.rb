b989191403f745a501612a1682477c6105cdcbf4
Victor Cordero <victordcordero@gmail.com>
2020-11-10 16:49:49 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   lowest_value = nil
 
 name_hash.each do |key, value|
-  if lowest_value = nil || lowest_value
+  if lowest_value = nil || value < lowest_value
     puts key
 end
 end
