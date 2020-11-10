8866a951cf5fd39a62f160c56d2c006c1f5b4403
Victor Cordero <victordcordero@gmail.com>
2020-11-10 16:47:37 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  lowest_key = nil
+  lowest_value = nil
 name_hash.each_with_index do |key, value, index|
   if value > 1
     puts key
